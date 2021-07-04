9ee156077ec920a8ebfb4f90e279caa3a87a5f56
slowed <rootslowed@gmail.com>
2021-07-04 12:52:30 -0300

    interface expense dao

---
+++ src/main/java/com/expenses/dao/IExpenseDAO.java
@@ -0,0 +1,18 @@
+package com.expenses.dao;
+
+import com.expenses.model.Category;
+import com.expenses.model.Expense;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IExpenseDAO {
+
+    Expense save(Expense expense);
+    Expense update(Expense expense);
+    void delete(Long id);
+    List<Expense> findAll();
+    Optional<Expense> findById(Long id);
+    List<Expense> findByCategory(Category category);
+
+}
